44b46912b3045f2019e28f3251a910113c99daff
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-10 16:37:50 -0500

    created Player and PlayerRoll classes

--- classes-practice/classes.py
+++ classes-practice/classes.py
@@ -9,9 +9,10 @@ class WorldCup:
 
 
 
-world_cup_games = WorldCup(teams="France vs England", location="Qatar", bracket_standing="Quarter Finals")
+# world_cup_games = WorldCup(teams="France vs England", location="Qatar", bracket_standing="Quarter Finals")
 
-print(world_cup_games)
+# print(world_cup_games)
+
 
 
 
@@ -31,4 +32,25 @@ class YourBet(WorldCup):
 your_bet = YourBet(person_bet="France WIN $200,000")
 your_bet.update_worldcup(teams="France vs Morocco", location="Qatar", bracket_standing="semi finals")
 
-print(your_bet)
+# print(your_bet)
+
+
+
+
+class Player:
+    def __init__(self, first_name = None, last_name = None, email = None, password = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+    def __str__(self):
+        return f"Account created: {self.first_name}, {self.last_name}, {self.email}, {self.password}"
+
+
+
+class PlayerRoll(Player):
+    def __init__(self, duelist, controller, initiator, sentinel):
+        self.duelist = duelist
+        self.controller = controller
+        self.initiator = initiator
+        self.sentinel = sentinel
